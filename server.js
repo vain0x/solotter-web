e5e997bfea5f3226cc7f4c004e6821a39b59f2af
vain0x <vainzerox@gmail.com>
2018-01-06 02:50:09 +0900

    Add main.js

--- server.js
+++ server.js
@@ -3,87 +3,90 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('solotter-web:server');
 var http = require('http');
 
-/**
- * Get port from environment and store in Express.
- */
+class SolotterServer {
+  run() {
+    /**
+     * Get port from environment and store in Express.
+     */
 
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+    this.port = this.normalizePort(process.env.PORT || '3000');
+    app.set('port', this.port);
 
-/**
- * Create HTTP server.
- */
+    /**
+     * Create HTTP server.
+     */
 
-var server = http.createServer(app);
+    this.server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
+    /**
+     * Listen on provided port, on all network interfaces.
+     */
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
+    this.server.listen(this.port);
+    this.server.on('error', err => this.onError(err));
+    this.server.on('listening', () => this.onListening());
   }
 
-  if (port >= 0) {
-    // port number
-    return port;
+  /**
+   * Normalize a port into a number, string, or false.
+   */
+  normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+
+    return false;
   }
 
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
+  /**
+   * Event listener for HTTP server "error" event.
+   */
+  onError(error) {
+    if (error.syscall !== 'listen') {
       throw error;
+    }
+
+    var bind = typeof this.port === 'string'
+      ? 'Pipe ' + this.port
+      : 'Port ' + this.port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  onListening() {
+    var addr = this.server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
   }
 }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+module.exports = SolotterServer;
