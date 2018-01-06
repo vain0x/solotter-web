2e49efa0079e3470d7d35e42a6fd6e6174ffbd01
vain0x <vainzerox@gmail.com>
2018-01-07 01:32:33 +0900

    Count visits as the sample of cookie-session

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, _next) {
+  req.session.visitCount = (req.session.visitCount || 0) + 1;
+
+  res.render('index', {
+    title: 'Express',
+    visitCount: req.session.visitCount,
+  });
 });
 
 module.exports = router;
