6f2afae5d2d08d7823986c9a6e5f220012d8959d
vain0x <vainzerox@gmail.com>
2018-01-20 01:53:34 +0900

    Support group-path notation

--- models/twitter-service.js
+++ models/twitter-service.js
@@ -152,6 +152,10 @@ class TwitterUserService {
     return users;
   }
 
+  async followers(screenName) {
+    throw new Error("not impl.");
+  }
+
   async listMembers(slug, ownerScreenName) {
     const option = {
       "slug": slug,
@@ -175,12 +179,40 @@ class TwitterUserService {
     return users;
   }
 
-  async members(slug) {
-    if (slug === "@friends") {
-      return await this.friends(this.user.screenName);
-    }
+  /**
+   * Parses a group path, a string to specify a collection of users, into an object.
+   * e.g.
+   *  @john/_friends (users that @john follows),
+   *  @john/_followers (users that follows @john),
+   *  @john/some-list-slug (users of tha list 'some-list-slug' owned by @john),
+   *  _friends (= @<login-user>/_friends),
+   *  some-slug (= @<login-user>/some-slug).
+   * @param {*} groupPath
+   * @returns { type, ownerScreenName, slug }
+   */
+  parseGroupPath(groupPath, defaultScreenName) {
+    const reg = new RegExp(String.raw`^(?:@([\w\d_-]+)/)?([\w\d_-]+)$`);
+    const match = groupPath.match(reg);
+    if (match === null) throw Error("Invalid list path.");
 
-    return await this.listMembers(slug, this.user.screenName);
+    const [, screenName, slug] = match;
+    const ownerScreenName = screenName || defaultScreenName;
+    const type = slug === "_friends" || slug === "_followers" ? slug.substr(1) : "list";
+    return { type, ownerScreenName, slug };
+  }
+
+  async members(slug) {
+    const group = this.parseGroupPath(slug, this.user.screenName);
+
+    switch (group.type) {
+      case "friends":
+        return await this.friends(group.ownerScreenName);
+      case "followers":
+        return await this.followers(group.ownerScreenName);
+      case "list":
+        return await this.listMembers(group.slug, group.ownerScreenName);
+      default: throw new Error();
+    }
   }
 
   chunkify(xs, limit) {
